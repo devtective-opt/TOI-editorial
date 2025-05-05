f53f09c39f4ab2209c1ce0b743aab4000f38eae8
Thanakrit Ponimthai <jadai.tk007@gmail.com>
2025-05-05 14:31:27 +0700

    Create TOI18_mountain.cpp

---
+++ TOI18_mountain.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct coor {
+    long long x , y ;
+};
+
+long long dp[501][501] ;
+coor node[501] ;
+int n, m ;
+
+long long dis(coor a , coor b){
+    return abs(a.x - b.x) + abs(a.y - b.y) ;
+}
+
+int main(){
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> n >> m ;
+    for(int i = 1 ; i <= n ; i ++ ){
+        cin >> node[i].x >> node[i].y ;
+    }
+    for(int i = 1 ; i <= n ; i++) for(int j = 1 ; j <= n ; j++) dp[i][j] = LLONG_MAX;
+    for(int i = 1 ; i <= n ; i++ ) dp[1][i] = dis(node[1] , node[i]) ;
+    long long MAX ;
+    for(int step = 2 ; step <= n ; step ++ ){
+        for(int i = 1 ; i <= n ; i ++ ){
+            dp[step][i] = dp[step - 1][i] ;
+            for(int j = 1 ; j <= n ; j ++ ){
+                MAX = max(dp[step - 1][j] , dis(node[i] , node[j])) ;
+                dp[step][i] = min(dp[step][i] , MAX) ;
+            }
+        }
+    }
+    long long res = 0 ;
+    while(m -- ){
+        long long range ; cin >> range ;
+        int l = 1 , r = n ;
+        while(l < r){
+            int mid = (l + r) >> 1 ;
+            if(dp[mid][n] <= range)r = mid ;
+            else l = mid + 1 ;
+        }
+        res += l ;
+    }
+    cout << res ;
+    return 0 ;
+}
